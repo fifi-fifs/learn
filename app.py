12c2bfea087a5e3c459309283b2be511d61a054b
Sofia Canossa <sofiacanossa5@gmail.com>
2025-04-03 23:29:09 +0100

    Introduced if __name__ == __main__

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 #  ____________________
 #  Import LIBRARIES
-from sqlmodel import Field, SQLModel, create_engine
+from sqlalchemy import Engine
+from sqlmodel import SQLModel, create_engine
 #  Import FILE
-# from . import models
+import models
 # from .db import engine
 #  ____________________
 
 
 
 
-class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
-    secret_name: str
-    age: int | None = None
+# Local DB
+sqlite_file_name:str = "database.db"
+sqlite_url:str= f"sqlite:///{sqlite_file_name}"
+# In-memory DB
+# sqlite_file_name:str = ""
+# sqlite_url:str= f"sqlite://{sqlite_file_name}"
+
+engine: Engine = create_engine(sqlite_url, echo=True)
 
 
+def create_db_and_tables() -> None:
+    SQLModel.metadata.create_all(engine)
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-engine = create_engine(sqlite_url, echo=True)
-
-SQLModel.metadata.create_all(engine)
+if __name__ == "__main__":
+    create_db_and_tables()
--- db.py
+++ db.py
@@ -1,14 +1,21 @@
 #  ____________________
 #  Import LIBRARIES
-from sqlmodel import create_engine
+from sqlmodel import SQLModel, create_engine
 #  Import FILE
 from . import models
 #  ____________________
 
 
 
+# Local DB
+sqlite_file_name:str = "database.db"
+sqlite_url:str= f"sqlite:///{sqlite_file_name}"
+# In-memory DB
+# sqlite_file_name:str = ""
+# sqlite_url:str= f"sqlite://{sqlite_file_name}"
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine: Engine = create_engine(sqlite_url, echo=True)
 
-engine = create_engine(sqlite_url, echo=True)
+
+def create_db_and_tables() -> None:
+    SQLModel.metadata.create_all(engine)
